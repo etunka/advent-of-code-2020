4d6acd7838de31989d4f5aecbd58524a5cdb701b
Ece Tunca <ece@Eces-MacBook-Air.local>
2020-12-11 16:42:20 +0100

    solved day2/part2

--- src/day2/index.js
+++ src/day2/index.js
@@ -14,3 +14,25 @@ readFileContent("day2/input.txt")
     }).length;
   })
   .then(logger);
+
+// PART 2
+
+readFileContent("day2/input.txt")
+  .then(parseLinesToArray)
+  .then((list) => {
+    return list.filter((row) => {
+      const colonIndex = row.indexOf(":");
+      const letter = row.charAt(colonIndex - 1);
+      const nums = row.match(/\d+/g).map(Number);
+      const password = row.slice(colonIndex + 1).trim();
+
+      const firstPosition = password[nums[0] - 1];
+      const secondPosition = password[nums[1] - 1];
+
+      return (
+        (firstPosition === letter || secondPosition === letter) &&
+        !(firstPosition === letter && secondPosition === letter)
+      );
+    }).length;
+  })
+  .then(logger);
--- src/utils.js
+++ src/utils.js
@@ -9,19 +9,6 @@ export function parseByEmptyLinesToArray(str) {
   return str.split("\n\n").filter((e) => e);
 }
 
-export function parseToMatrix(str) {
-  return parseLinesToArray(str).map((e) => e.split(""));
-}
-
-// Greatest common devisor
-// export function gcd(a: number, b: number) {
-//   return !b ? a : gcd(b, a % b);
-// }
-
-export function calcAngleDegrees(dx, dy) {
-  return (Math.atan2(dy, dx) * 180) / Math.PI;
-}
-
 export async function readFileContent(fileName = "testData.txt") {
   const file = await fs.readFile(path.join(__dirname, fileName));
 
